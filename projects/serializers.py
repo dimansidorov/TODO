9e8d4be33d00930989c4ce85ee60bff57e64e1e1
dimansidorov <dimkasidorow@gmail.com>
2023-01-14 19:12:27 +0100

    add filters, pagination

--- projects/serializers.py
+++ projects/serializers.py
@@ -4,14 +4,15 @@ from rest_framework.serializers import ModelSerializer
 
 
 class ProjectModelSerializer(ModelSerializer):
-    # creators = UserModelSerializer(many=True)
+    creators = UserModelSerializer(many=True)
     class Meta:
         model = Project
-        fields = ('title', 'link', 'creators')
+        fields = ('id', 'title', 'link', 'creators')
 
 
 class TodoModelSerializer(ModelSerializer):
+    creator = UserModelSerializer()
 
     class Meta:
         model = ToDo
-        fields = ('project', 'text', 'updated_at', 'created_at', 'active', 'creator')
+        fields = ('id', 'project', 'text', 'updated_at', 'created_at', 'active', 'creator')
