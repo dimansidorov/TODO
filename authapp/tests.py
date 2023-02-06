41d6226cd5c1974fb2ac04fecae57de48bdc1efe
dimansidorov <dimkasidorow@gmail.com>
2023-02-06 12:44:58 +0100

    add tests

--- authapp/tests.py
+++ authapp/tests.py
@@ -1,3 +1,10 @@
-from django.test import TestCase
-
+from rest_framework import status
+from rest_framework.test import APITestCase
 # Create your tests here.
+
+
+class TestToDoModelViewSet(APITestCase):
+
+    def test_get_list(self):
+        response = self.client.get('/api/users/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
