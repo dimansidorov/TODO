ecae7d180506685ca8f16c27ad601256b1b96456
Dmitry Sidorov <93219835+dimansidorov@users.noreply.github.com>
2023-02-24 21:40:23 +0300

    Lesson 12 (#13)
    * create command create_data,create requirements.txt
    
    * create docker-compose.yml and other files
    
    * updated nginx.conf

--- todo/settings.py
+++ todo/settings.py
@@ -82,10 +82,21 @@ WSGI_APPLICATION = 'todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'todo',
+        'USER': 'admin',
+        'PASSWORD': 'admin',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
